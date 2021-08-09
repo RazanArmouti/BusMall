465ca38a84728902929d4f00b7636bd3d87dd396
Razan Armouti <razanarmote@yahoo.com>
2021-08-09 16:35:37 +0300

    lab12-chart

--- js/app.js
+++ js/app.js
@@ -1,24 +1,41 @@
 'use strict';
 // Variables Declaration
-let imagesArray = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg',
-  'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg',
-  'water-can.jpg', 'wine-glass.jpg'];
+let imagesArray = ['bag.jpg',
+  'banana.jpg',
+  'bathroom.jpg',
+  'boots.jpg',
+  'breakfast.jpg',
+  'bubblegum.jpg',
+  'chair.jpg',
+  'cthulhu.jpg',
+  'dog-duck.jpg',
+  'dragon.jpg',
+  'pen.jpg',
+  'pet-sweep.jpg',
+  'scissors.jpg',
+  'shark.jpg',
+  'sweep.png',
+  'tauntaun.jpg',
+  'unicorn.jpg',
+  'water-can.jpg',
+  'wine-glass.jpg'];
 
 const imgSec = document.getElementById('imgSec');
 let firstImage = document.getElementById('firstImage');
 let secondImage = document.getElementById('secondImage');
 let thirdImage = document.getElementById('thirdImage');
 
-let allSplitArray = [];
-
 let numberOfRound = 25;
-let totalOfShow = 0;
 let totalOfClicked = 0;
-let clikedFlag;
-let imageFlag='';
 
-let clickedresult = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-let seenResult    = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let firstRandom = 0;
+let secondRandom = 0;
+let thirdRandom = 0;
+
+let oldFirstRandom = 0;
+let oldSecondRandom = 0;
+let oldThirdRandom = 0;
+
 
 
 //constructor
@@ -34,76 +51,46 @@ function getRandomNo(max, min) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 function SplitImges() {
-  imgConstructor.allSplitArray = [];
+
   for (let i = 0; i < imagesArray.length; i++) {
     new imgConstructor(imagesArray[i].split('.')[0], imagesArray[i]);
 
   }
 
-  //console.log(imgConstructor.allSplitArray);
 }
 function render() {
 
   SplitImges();
 
-  let firstRandom = getRandomNo(0, imagesArray.length - 1);
-  let secondRandom = getRandomNo(0, imagesArray.length - 1);
-  let thirdRandom = getRandomNo(0, imagesArray.length - 1);
-  while (firstRandom === secondRandom || firstRandom === thirdRandom || secondRandom === thirdRandom) {
+  do {
     firstRandom = getRandomNo(0, imagesArray.length - 1);
     secondRandom = getRandomNo(0, imagesArray.length - 1);
     thirdRandom = getRandomNo(0, imagesArray.length - 1);
-  }
+
+    // console.log(firstRandom,secondRandom,thirdRandom,oldFirstRandom,oldSecondRandom,oldThirdRandom);
+
+  } while ((firstRandom === secondRandom || firstRandom === thirdRandom || secondRandom === thirdRandom) ||
+  (oldFirstRandom === firstRandom || oldFirstRandom === secondRandom || oldFirstRandom === thirdRandom) ||
+  (oldSecondRandom === firstRandom || oldSecondRandom === secondRandom || oldSecondRandom === thirdRandom) ||
+    (oldThirdRandom === firstRandom || oldThirdRandom === secondRandom || oldThirdRandom === thirdRandom));
+  oldFirstRandom = firstRandom;
+  oldSecondRandom = secondRandom;
+  oldThirdRandom = thirdRandom;
+
+
+
 
   firstImage.src = imgConstructor.allSplitArray[firstRandom].imageLoc;
   secondImage.src = imgConstructor.allSplitArray[secondRandom].imageLoc;
   thirdImage.src = imgConstructor.allSplitArray[thirdRandom].imageLoc;
 
-  seenResult[firstRandom] += 1;
-  seenResult[secondRandom] += 1;
-  seenResult[thirdRandom] += 1;
-  //console.log(firstRandom,secondRandom,thirdRandom);
 
-  //   imgConstructor.allSplitArray[firstRandom].seen++;
-  //   imgConstructor.allSplitArray[secondRandom].seen++;
-  //   imgConstructor.allSplitArray[thirdRandom].seen++;
-
-  if (clikedFlag === true) {
-
-    if (imageFlag === 'firstImage') {
-      //imgConstructor.allSplitArray[firstRandom].vote++;
-      //console.log(imageFlag,firstRandom);
-
-      clickedresult[firstRandom] += 1;
-
-
-    }
-
-    else if (imageFlag === 'secondImage') {
-      // imgConstructor.allSplitArray[secondRandom].vote++;
-      // console.log(imageFlag,secondRandom);
-
-      clickedresult[secondRandom] += 1;
-
-
-    }
-    else if (imageFlag === 'thirdImage') {
-      // imgConstructor.allSplitArray[thirdRandom].vote++;
-      //console.log(imageFlag,thirdRandom);
-
-      clickedresult[thirdRandom] += 1;
-
-
-    }
-
-
-  }
-
-  totalOfShow++;
-  imageFlag='';
+  imgConstructor.allSplitArray[firstRandom].seen++;
+  imgConstructor.allSplitArray[secondRandom].seen++;
+  imgConstructor.allSplitArray[thirdRandom].seen++;
 
 }
-
+imgConstructor.allSplitArray = [];
 
 const resForm = document.getElementById('resForm');
 function renderresult() {
@@ -116,25 +103,91 @@ function renderresult() {
   resForm.addEventListener('submit', SubmitHandlerforRender);
 
 }
+
 function SubmitHandlerforRender(event) {
   event.preventDefault();
   const listDiv = document.getElementById('listDiv');
-  let olElement = document.createElement('ol');
-  listDiv.appendChild(olElement);
+  let ulElement = document.createElement('Ul');
+  listDiv.appendChild(ulElement);
   for (let res = 0; res < imagesArray.length; res++) {
-    imgConstructor.allSplitArray[res].seen =seenResult[res];
-    imgConstructor.allSplitArray[res].seen =seenResult[res];
-    imgConstructor.allSplitArray[res].seen =seenResult[res];
-    imgConstructor.allSplitArray[res].vote =clickedresult[res];
-    imgConstructor.allSplitArray[res].vote = clickedresult[res];
-    imgConstructor.allSplitArray[res].vote = clickedresult[res];
     let liElement = document.createElement('li');
     liElement.textContent = `${imgConstructor.allSplitArray[res].name} had ${imgConstructor.allSplitArray[res].vote} votes, and was seen ${imgConstructor.allSplitArray[res].seen} times.`;
-    olElement.appendChild(liElement);
-    //console.log(liElement.textContent);
-
+    ulElement.appendChild(liElement);
   }
-  //console.log(clickedresult);
+  resForm.removeEventListener('submit', SubmitHandlerforRender);
+
+}
+
+function chartRender() {
+  let nameArr=[];
+  let seenArr=[];
+  let voteArr=[];
+  let ctx = document.getElementById('myChart').getContext('2d');
+  for(let x=0;x<imagesArray.length;x++){
+    nameArr.push( imgConstructor.allSplitArray[x].name);
+    seenArr.push(imgConstructor.allSplitArray[x].seen);
+    voteArr.push( imgConstructor.allSplitArray[x].vote);
+    
+  }
+  
+  let myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels:nameArr,
+      datasets: [{
+        label: '# of Seen',
+        data:seenArr ,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      },
+
+      {
+        label: '# of Votes',
+        data:voteArr,
+        backgroundColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+
+          
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
 
 
 }
@@ -143,34 +196,30 @@ imgSec.addEventListener('click', sectionClickHandler);
 function sectionClickHandler(e) {
   if ((e.target.id === 'firstImage' || e.target.id === 'secondImage' || e.target.id === 'thirdImage') && totalOfClicked < numberOfRound) {
 
-    imageFlag = e.target.id;
-    clikedFlag = true;
-    //console.log(imageFlag);
+    if (e.target.id === 'firstImage') {
+      imgConstructor.allSplitArray[firstRandom].vote++;
+
+    }
+
+    else if (e.target.id === 'secondImage') {
+      imgConstructor.allSplitArray[secondRandom].vote++;
+
+    }
+    else if (e.target.id === 'thirdImage') {
+      imgConstructor.allSplitArray[thirdRandom].vote++;
+
+    }
+
     render();
 
-    imageFlag='';
-    clikedFlag = false;
     totalOfClicked++;
   }
-  else {
+
+  if (totalOfClicked >= numberOfRound) {
     renderresult();
     imgSec.removeEventListener('click', sectionClickHandler);
+    chartRender();
 
   }
-  //console.log(clickedTime);
-
 }
-
 render();
-
-
-
-
-
-
-
-
-
-
-
-
